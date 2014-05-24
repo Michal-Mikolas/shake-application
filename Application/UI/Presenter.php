d691978c6c01f8e1ac16e1297058cb6cb6ec79f2
Michal Mikolas <nanuqcz@gmail.com>
2014-05-25 00:01:56 +0200

    Initial commit.

---
+++ Application/UI/Presenter.php
@@ -0,0 +1,52 @@
+<?php
+namespace Shake\Application\UI;
+
+use \Shake\Utils\Strings,
+	\Shake\VisualPaginator;
+use \Nette;
+
+
+/**
+ * Application\UI\Presenter
+ *
+ * @author  Michal Mikoláš <nanuqcz@gmail.com>
+ */
+class Presenter extends Nette\Application\UI\Presenter
+{
+
+	protected function paginate($data, $service)
+	{
+		return $service->applyLimit(
+			$data, 
+			$this['paginator']->paginator->itemsPerPage, 
+			$this['paginator']->paginator->offset
+		);
+	}
+
+
+
+	public function &__get($name)
+	{
+		// Default behavior
+		try {
+			return parent::__get($name);
+
+		// Automatic service getter from context
+		} catch (\Nette\MemberAccessException $e) {
+			// Repository
+			if (strrpos($name, 'Repository') == (strlen($name) - 10)) {
+				$repository = $this->context->$name;
+				return $repository;
+			}
+			
+			// Service
+			if (strrpos($name, 'Service') == (strlen($name) - 7)) {
+				$service = $this->context->$name;
+				return $service;
+			}
+
+			throw $e;
+		}
+	}
+
+}
